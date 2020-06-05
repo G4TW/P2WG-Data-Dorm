61ce64fdd72f9ac78acdb8833bf8261db71ec1dc
Gamers <minecraftbyjordan1@gmail.com>
2020-06-05 14:06:10 -0400

    Update globalbanlist.lua

--- globalbanlist.lua
+++ globalbanlist.lua
@@ -2,5 +2,5 @@
                                                    --PLAY2WIN GAMES GLOBAL BANLIST--
                                              id based so players cant change their name
                                                  may take a few minutes to update lel                                                  ]]
---No bans... yet
+--No bans... yet!
 515820109
